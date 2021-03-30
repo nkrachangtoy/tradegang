70ec5f2d6e506d5dd7dfdc599f4ef874cc15a275
Nusorn Krachangtoy <nkrachangtoy@gmail.com>
2021-03-30 02:15:43 -0700

    install axios and create .env to store api_key

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
+import {View, Text} from 'react-native'
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [data, setData] = useState([])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
