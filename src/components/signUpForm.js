819cfb0f9130c1580472c48f87d5d4a7e22c3af2
Nusorn Krachangtoy <nkrachangtoy@gmail.com>
2021-03-30 18:11:15 -0700

    install firebase auth dependencies

---
+++ src/components/signUpForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const signUpForm = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default signUpForm
+
+const styles = StyleSheet.create({})
